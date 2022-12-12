7a220a160401a6cec49633618475e7d8f13ac11c
Ian Wixom <107264322+1anw@users.noreply.github.com>
2022-12-11 22:11:53 -0600

    Update irf.py

--- code/irf.py
+++ code/irf.py
@@ -1,7 +1,7 @@
-# FINAL.py
+# irf.py
 #   Improved Regula Falsi method by Soumen Shaw and Basudeb Mukhopadhyay, with parameter k = f(c)/f(a or b)
 #   coded in python
-# By: Andy Shapiro
+# By: Andy Shapiro and Ian Wixom
 
 import math
 import matplotlib.pyplot as plt
@@ -79,36 +79,30 @@ def main():
     tolerance = 1E-5
 
     bounds_1 = [[-2*U_1, -U_2 - tolerance], [-U_2 + tolerance, U_1 - tolerance], [U_1 + tolerance, 2*U_1]]
-    bounds_2 = [[-1.1, -0.9], [0.75, 0.95], [1.05, 1.15]]
+    bounds_2 = [[-1.1, -0.9], [0.75, 0.95], [1.05, 1.25]]
     print("original, guessed bounds: ")
     original_result = method(bounds_1, tolerance)
     print("approximated bounds: ")
     bounded_result = method(bounds_2, tolerance)
 
-    plt.plot(original_result[0])
-    plt.plot(original_result[1])
-    plt.plot(original_result[2])
-    
-    plt.xlabel('iterations')
-    plt.ylabel('log error')
-    plt.yscale('log')
+    plt.plot(original_result[0], color = 'r')
+    plt.plot(original_result[1], color = 'b')
+    plt.plot(original_result[2], color = 'g')
 
-    plt.legend(["Between [-1.975, -0.01251]", "Between [0.01251, 0.98749]", " Between [0.98751, 1.975]"])
+    plt.legend(["L_1", "L_2", " L_3"])
     
-    plt.title('Improved RF\'s Method Approximation with Original Bounds')
-    
-    plt.show()
+    plt.title('IRF Method Approx. with Original Bounds, New Bounds as Dashed')
 
-    plt.plot(bounded_result[0])
-    plt.plot(bounded_result[1])
-    plt.plot(bounded_result[2])
+    plt.plot(bounded_result[0], color =  'r', ls = '--')
+    plt.plot(bounded_result[1], color = 'b', ls = '--')
+    plt.plot(bounded_result[2], color = 'g', ls = '--')
+
+    plt.axhline(y = 1e-5, color = 'k', linestyle = ':')
+    plt.grid(True)
     
     plt.xlabel('iterations')
     plt.ylabel('log error')
     plt.yscale('log')  
-    plt.legend(["Between [-1.1, -0.9]", "Between [0.75, 0.95]", " Between [1.05, 1.15]"])
-
-    plt.title('Improved RF\'s Method Approximation with New Bounds')
     
     plt.show()
     
